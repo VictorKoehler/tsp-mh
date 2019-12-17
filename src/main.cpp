3afe456b5d6e6f298e6f7855617c16e9670dd8b9
Victor <koehlervictor@cc.ci.ufpb.br>
2019-12-17 16:31:57 -0300

    Data Fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,37 @@
-#include "readData.h"
 #include <fstream>
 #include <iostream>
+#include "readData.h"
 
 using namespace std;
 
-double ** matrizAdj; // matriz de adjacencia
-int dimension; // quantidade total de vertices
+double** matrizAdj;  // matriz de adjacencia
+uint dimension;      // quantidade total de vertices
 
 void printData();
+void realignData();
 
 int main(int argc, char** argv) {
-
-    readData(argc, argv, &dimension, &matrizAdj);
-    printData();
+    int dim;
+    readData(argc, argv, &dim, &matrizAdj);
+    dimension = uint(dim);
+    realignData();
     return 0;
-
-    
-
 }
 
 void printData() {
-  cout << "dimension: " << dimension << endl;
-  for (size_t i = 1; i <= dimension; i++) {
-    for (size_t j = 1; j <= dimension; j++) {
-      cout << matrizAdj[i][j] << " ";
+    cout << "dimension: " << dimension << endl;
+    for (uint i = 1; i <= dimension; i++) {
+        for (uint j = 1; j <= dimension; j++) {
+            cout << matrizAdj[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
+}
+
+void realignData() {
+    for (uint i = 0; i < dimension; i++) {
+        for (uint j = 0; j < dimension; j++) {
+            matrizAdj[i][j] = matrizAdj[i + 1][j + 1];
+        }
+    }
 }
